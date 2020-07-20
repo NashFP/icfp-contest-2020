1a180d374b4a7b64835245e18653b2c5fa724f42
Mark Wutka <mark.wutka@banyanhills.com>
2020-07-19 21:47:34 -0500

    Shoot if they are in range

--- src/Attacker.hs
+++ src/Attacker.hs
@@ -7,15 +7,20 @@ getAttackerCommands ::GameState -> [Command]
 getAttackerCommands (GameState _ _ shipsAndCommands) =
   let enemies = getShipData DefenderRole shipsAndCommands in
   let myShips = getShipData AttackerRole shipsAndCommands in
-  map (getAttackerCommand enemies) myShips
+  concatMap (getAttackerCommand enemies) myShips
 
-getAttackerCommand :: [ShipData] -> ShipData -> Command
-getAttackerCommand enemies (ShipData shipId (myX,myY) myVel) =
---  let (ShipData _ (enemyX,enemyY) _) = findClosest (myX,myY) enemies in
---  let enemyDist = distanceSquared (myX,myY) (enemyX,enemyY) in
---  if enemyDist < 200 then
---    ShootCommand shipId (enemyX,enemyY) (IntValue 0)
---  else
-    AccelerateCommand shipId $ computeOrbitVector (myX,myY)
+getAttackerCommand :: [ShipData] -> ShipData -> [Command]
+getAttackerCommand enemies (ShipData shipId (myX,myY) (myXVel,myYVel)) =
+    let (orbitX,orbitY) = computeOrbitVector (myX,myY) in
+    let xAccel = if myXVel > 8 then 1 else if myXVel < -8 then -1 else orbitX in
+    let yAccel = if myYVel > 8 then 1 else if myYVel < -8 then -1 else orbitY in
+    let cmds = [AccelerateCommand shipId $ (xAccel,yAccel)] in
+
+    let (ShipData _ (enemyX,enemyY) _) = findClosest (myX,myY) enemies in
+    let enemyDist = distanceSquared (myX,myY) (enemyX,enemyY) in
+    if enemyDist < 2000 then
+      (ShootCommand shipId (enemyX,enemyY) (IntValue 0)) : cmds
+    else
+      cmds
 
 
--- src/Defender.hs
+++ src/Defender.hs
@@ -8,13 +8,20 @@ getDefenderCommands (GameState _ _ shipsAndCommands) =
   let enemies = getShipData AttackerRole shipsAndCommands in
   let myShips = getShipData DefenderRole shipsAndCommands in
 
-  map (getDefenderCommand enemies) myShips
+  concatMap (getDefenderCommand enemies) myShips
 
-getDefenderCommand :: [ShipData] -> ShipData -> Command
-getDefenderCommand enemies (ShipData shipId (myX,myY) myVel) =
---  let (ShipData _ (enemyX,enemyY) _) = findClosest (myX,myY) enemies in
---  let enemyDist = distanceSquared (myX,myY) (enemyX,enemyY) in
---  if enemyDist < 200 then
---    ShootCommand shipId (enemyX,enemyY) (IntValue 0)
---  else
-    AccelerateCommand shipId $ computeOrbitVector (myX,myY)
+getDefenderCommand :: [ShipData] -> ShipData -> [Command]
+getDefenderCommand enemies (ShipData shipId (myX,myY) (myXVel,myYVel)) =
+    let (orbitX,orbitY) = computeOrbitVector (myX,myY) in
+    let xAccel = if myXVel > 8 then 1 else if myXVel < -8 then -1 else orbitX in
+    let yAccel = if myYVel > 8 then 1 else if myYVel < -8 then -1 else orbitY in
+    let cmds = [AccelerateCommand shipId $ (xAccel,yAccel)] in
+
+    let (ShipData _ (enemyX,enemyY) _) = findClosest (myX,myY) enemies in
+    let enemyDist = distanceSquared (myX,myY) (enemyX,enemyY) in
+    if enemyDist < 2000 then
+      (ShootCommand shipId (enemyX,enemyY) (IntValue 0)) : cmds
+    else
+      cmds
+
+
