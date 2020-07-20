e4abc3bee31dc1b5bed0348f7c5cce99b9e1bd29
Mark Wutka <mark.wutka@banyanhills.com>
2020-07-19 21:33:14 -0500

    Control speed

--- src/Defender.hs
+++ src/Defender.hs
@@ -8,13 +8,16 @@ getDefenderCommands (GameState _ _ shipsAndCommands) =
   let enemies = getShipData AttackerRole shipsAndCommands in
   let myShips = getShipData DefenderRole shipsAndCommands in
 
-  map (getDefenderCommand enemies) myShips
+  concatMap (getDefenderCommand enemies) myShips
 
-getDefenderCommand :: [ShipData] -> ShipData -> Command
-getDefenderCommand enemies (ShipData shipId (myX,myY) myVel) =
+getDefenderCommand :: [ShipData] -> ShipData -> [Command]
+getDefenderCommand enemies (ShipData shipId (myX,myY) (myXVel,myYVel)) =
 --  let (ShipData _ (enemyX,enemyY) _) = findClosest (myX,myY) enemies in
 --  let enemyDist = distanceSquared (myX,myY) (enemyX,enemyY) in
 --  if enemyDist < 200 then
 --    ShootCommand shipId (enemyX,enemyY) (IntValue 0)
 --  else
-    AccelerateCommand shipId $ computeOrbitVector (myX,myY)
+    let (orbitX,orbitY) = computeOrbitVector (myX,myY) in
+    let xAccel = if myXVel > 8 then 1 else if myXVel < -8 then -1 else orbitX in
+    let yAccel = if myYVel > 8 then 1 else if myYVel < -8 then -1 else orbitY in
+    [AccelerateCommand shipId $ (xAccel,yAccel)]
