c6bdf163681652a6aec95ce67fd54c49aa28c955
Mark Wutka <mark.wutka@banyanhills.com>
2020-07-20 01:07:30 -0500

    Allow defender to clone

--- src/Defender.hs
+++ src/Defender.hs
@@ -8,10 +8,10 @@ getDefenderCommands (GameState ticks _ shipsAndCommands) =
   let enemies = getShipData AttackerRole shipsAndCommands in
   let myShips = getShipData DefenderRole shipsAndCommands in
 
-  concatMap (getDefenderCommand ticks enemies) myShips
+  concatMap (getDefenderCommand ticks (length myShips) enemies) myShips
 
-getDefenderCommand :: Integer -> [ShipData] -> ShipData -> [Command]
-getDefenderCommand ticks enemies (ShipData shipId (myX,myY) (myXVel,myYVel) energy) =
+getDefenderCommand :: Integer -> Int -> [ShipData] -> ShipData -> [Command]
+getDefenderCommand ticks numShips enemies (ShipData shipId (myX,myY) (myXVel,myYVel) energy) =
     let (orbitX,orbitY) = computeOrbitVector (myX,myY) in
     let xAccel = if myXVel > 5 then 1 else if myXVel < -5 then -1 else orbitX in
     let yAccel = if myYVel > 5 then 1 else if myYVel < -5 then -1 else orbitY in
@@ -25,8 +25,12 @@ getDefenderCommand ticks enemies (ShipData shipId (myX,myY) (myXVel,myYVel) ener
                     else if (myY > 0) && (myYVel < 0) 
                          then -1 else 0
                     else yAccel in
-    let cmds = [AccelerateCommand shipId $ (xAccel',yAccel')] in
-
+    let accelCommand = [AccelerateCommand shipId $ (xAccel',yAccel')] in
+    let cmds = if shipId < 2 && ticks > 10 && numShips < 15 then
+                 (SpawnCommand shipId [0,0,0,1]) : accelCommand
+               else
+                 accelCommand
+               in
     cmds
 --    let (ShipData _ (enemyX,enemyY) _) = findClosest (myX,myY) enemies in
 --    if ticks `rem` 5 == 0 then
